cbd5eba79639c47b52aa6bbc3ee0fbbd22975cbd
Michael Clifford <cliffom@gmail.com>
2021-10-13 07:00:16 -0700

    Setup for easier testing

--- src/handlers/api/test/main.go
+++ src/handlers/api/test/main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func Handler(request events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
+// Handler is the main function that handles the request
+func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
 		StatusCode:        http.StatusOK,
 		Headers:           map[string]string{},
 		MultiValueHeaders: map[string][]string{},
-		Body:              "Your request was received at " + request.RequestContext.Time + ".",
+		Body:              "Your request was received at " + req.RequestContext.Time + ".",
 		IsBase64Encoded:   false,
 	}, nil
 }
 
+// handler is a wrapper to Handler and allows us to setup dependencies
+// for easier testing of Handler
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
+	return Handler(ctx, req)
+}
+
 func main() {
-	lambda.Start(Handler)
+	lambda.Start(handler)
 }
