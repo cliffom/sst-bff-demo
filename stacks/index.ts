a9ab2fe9045e9cb3b64c7631a7400d980ccfb6cd
Michael Clifford <cliffom@gmail.com>
2021-10-14 15:53:09 -0700

    Update removal policy

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,8 +1,15 @@
 import * as sst from '@serverless-stack/resources';
+import {RemovalPolicy} from '@aws-cdk/core';
 import ApolloStack from './ApolloStack';
 import APIStack from './APIStack';
 
 export default function main(app: sst.App): void {
+  // Remove all resources when non-prod stages are removed
+  // https://docs.serverless-stack.com/constructs/App#setting-a-default-removal-policy
+  if (app.stage !== 'prod') {
+    app.setDefaultRemovalPolicy(RemovalPolicy.DESTROY);
+  }
+
   // Create the API stack where all services are defined
   const apiStack = new APIStack(app, 'api-stack');
 
