799012e696382d9815bed4b2d76e5dd42be06982
Michael Clifford <cliffom@gmail.com>
2021-10-13 19:16:41 -0700

    Start stubbing out user methods

--- stacks/APIStack.ts
+++ stacks/APIStack.ts
@@ -10,16 +10,34 @@ export default class APIStack extends sst.Stack {
       runtime: 'go1.x',
     });
 
+    const table = new sst.Table(this, 'Storage', {
+      fields: {
+        PK: sst.TableFieldType.STRING,
+        SK: sst.TableFieldType.STRING,
+      },
+      primaryIndex: {partitionKey: 'PK', sortKey: 'SK'},
+    });
+
     const testHandler = new sst.Function(this, 'testHandler', {
       handler: 'src/handlers/api/test',
+    });
+
+    const usersHandler = new sst.Function(this, 'usersHandler', {
+      handler: 'src/handlers/api/users',
+      environment: {
+        TABLE_NAME: table.dynamodbTable.tableName,
+      },
     });
 
     // Create a HTTP API
     this.api = new sst.Api(this, 'Api', {
       routes: {
         'GET /test': testHandler,
-        'GET /user': 'src/handlers/api/user',
+        'GET /user': usersHandler,
+        'POST /user': usersHandler,
       },
     });
+
+    this.api.attachPermissions([table]);
   }
 }
