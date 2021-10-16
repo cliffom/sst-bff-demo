c72b9d6c59108429ee1aa26c38aebd043bbeb1b4
Michael Clifford <cliffom@gmail.com>
2021-10-15 19:07:35 -0700

    Don't return err if conflict

--- src/handlers/api/users/main.go
+++ src/handlers/api/users/main.go
@@ -50,7 +50,7 @@ func main() {
 
 func createUser(dbSvc *dynamodb.DynamoDB, userToCreate User) (events.APIGatewayProxyResponse, error) {
 	if err := CreateUser(dbSvc, &userToCreate); err != nil {
-		return response(http.StatusConflict, err.Error()), err
+		return response(http.StatusConflict, err.Error()), nil
 	}
 
 	u, _ := json.Marshal(userToCreate)
