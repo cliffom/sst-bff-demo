6adf596da50db06e479feafa0a6a97e90b93656d
Michael Clifford <cliffom@gmail.com>
2021-10-19 22:12:13 -0700

    Single Table (#4)

--- stacks/index.ts
+++ stacks/index.ts
@@ -3,6 +3,7 @@ import {RemovalPolicy} from '@aws-cdk/core';
 import ApolloStack from './ApolloStack';
 import APIStack from './APIStack';
 import AuthStack from './AuthStack';
+import TableStack from './TableStack';
 
 export default function main(app: sst.App): void {
   // Remove all resources when non-prod stages are removed
@@ -14,10 +15,16 @@ export default function main(app: sst.App): void {
   // Create our Auth stack that defines our Cognito pool and client
   const authStack = new AuthStack(app, 'auth-stack');
 
+  // Create our single DynamoDB table
+  const tableStack = new TableStack(app, 'table-stack');
+
   // Create the API stack where all services are defined
-  const apiStack = new APIStack(app, 'api-stack', {authorizer: authStack.authorizer});
+  const apiStack = new APIStack(app, 'api-stack', {
+    authorizer: authStack.authorizer,
+    table: tableStack.table,
+  });
 
   // Create the Apollo stack where the Apollo Server is defined
-  /* eslint-disable no-new */
+  /* eslint-disable-next-line no-new */
   new ApolloStack(app, 'apollo-stack', {api: apiStack.api});
 }
