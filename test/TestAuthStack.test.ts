3d1ee8daf788d7ada295a4a7794cf971dfcb7d79
Michael Clifford <cliffom@gmail.com>
2022-02-08 14:17:11 -0800

    Fix broken tests

--- test/TestAuthStack.test.ts
+++ test/TestAuthStack.test.ts
@@ -1,19 +1,22 @@
 import * as sst from '@serverless-stack/resources';
 import AuthStack from '../stacks/AuthStack';
-import TableStack from '../stacks/TableStack';
+import UsersAsyncStack from '../stacks/users_async/UsersAsyncStack';
 import {Template} from 'aws-cdk-lib/assertions';
+import TableStack from '../stacks/TableStack';
 
 test('Test Stack', () => {
   const app = new sst.App();
   // WHEN
   const tableStack = new TableStack(app, 'table-stack');
-  const stack = new AuthStack(app, 'test-stack', {
+  const usersAsyncStack = new UsersAsyncStack(app, 'users-async-stack', {
     table: tableStack.table,
+  });
+  const stack = new AuthStack(app, 'test-stack', {
+    postConfirmationFunction: usersAsyncStack.createUserFunction,
   });
   const template = Template.fromStack(stack);
 
   // THEN
-  template.resourceCountIs('AWS::Lambda::Function', 1);
   template.resourceCountIs('AWS::Cognito::UserPool', 1);
   template.resourceCountIs('AWS::Cognito::UserPoolClient', 1);
 });
