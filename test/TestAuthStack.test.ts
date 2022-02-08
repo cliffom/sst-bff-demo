9411e304533a4660cf1964be79cea25e21568462
Michael Clifford <cliffom@gmail.com>
2022-02-08 13:15:38 -0800

    Fixed tests

--- test/TestAuthStack.test.ts
+++ test/TestAuthStack.test.ts
@@ -1,15 +1,19 @@
 import * as sst from '@serverless-stack/resources';
 import AuthStack from '../stacks/AuthStack';
-import { Template } from 'aws-cdk-lib/assertions';
+import TableStack from '../stacks/TableStack';
+import {Template} from 'aws-cdk-lib/assertions';
 
 test('Test Stack', () => {
   const app = new sst.App();
   // WHEN
-  const stack = new AuthStack(app, 'test-stack');
-  const template = Template.fromStack(stack)
+  const tableStack = new TableStack(app, 'table-stack');
+  const stack = new AuthStack(app, 'test-stack', {
+    table: tableStack.table,
+  });
+  const template = Template.fromStack(stack);
 
   // THEN
-  template.resourceCountIs("AWS::Lambda::Function", 1);
+  template.resourceCountIs('AWS::Lambda::Function', 1);
   template.resourceCountIs('AWS::Cognito::UserPool', 1);
   template.resourceCountIs('AWS::Cognito::UserPoolClient', 1);
 });
--- test/TestUsersAPIStack.test.ts
+++ test/TestUsersAPIStack.test.ts
@@ -1,32 +1,29 @@
 import * as sst from '@serverless-stack/resources';
 import UsersAPIStack from '../stacks/users_api/UsersAPIStack';
-import AuthStack from '../stacks/AuthStack'
-import TableStack from '../stacks/TableStack'
-import { Template } from "aws-cdk-lib/assertions";
+import AuthStack from '../stacks/AuthStack';
+import TableStack from '../stacks/TableStack';
+import {Template} from 'aws-cdk-lib/assertions';
 
 test('Test UsersAPIStack', () => {
   const app = new sst.App();
 
   // WHEN
-  const authStack = new AuthStack(app, 'auth-stack');
   const tableStack = new TableStack(app, 'table-stack');
+  const authStack = new AuthStack(app, 'auth-stack', {
+    table: tableStack.table,
+  });
   const stack = new UsersAPIStack(app, 'test-stack', {
     authorizer: authStack.authorizer,
-    table: tableStack.table
+    table: tableStack.table,
   });
-  const template = Template.fromStack(stack)
+  const template = Template.fromStack(stack);
 
   // THEN
   template.resourceCountIs('AWS::Lambda::Function', 1);
-  template.resourceCountIs('AWS::ApiGatewayV2::Route', 2);
-  
-  template.hasResourceProperties('AWS::ApiGatewayV2::Route', {
-    RouteKey: 'GET /users/me',
-    AuthorizationType: 'JWT'
-  });
+  template.resourceCountIs('AWS::ApiGatewayV2::Route', 1);
 
   template.hasResourceProperties('AWS::ApiGatewayV2::Route', {
-    RouteKey: 'POST /users',
-    AuthorizationType: 'JWT'
+    RouteKey: 'GET /users/me',
+    AuthorizationType: 'JWT',
   });
 });
